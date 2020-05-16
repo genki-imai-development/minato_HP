4c2b9031d69088ce9371bdbc63cb6d60380dedcd
genki-imai-development <genki-imai-development@gmail.com>
2020-05-17 01:28:57 +0900

    トップにセクション要素を追加

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,27 +1,31 @@
 <template>
-  <carousel
-    :per-page="1"
-    :autoplay="true"
-    :loop="true"
-    :pagination-padding="5"
-    :autoplay-timeout="4000"
-  >
-    <slide>
-      <img src="@/assets/images/slide1.jpg" />
-    </slide>
-    <slide>
-      <img src="@/assets/images/slide2.jpg" />
-    </slide>
-    <slide>
-      <img src="@/assets/images/slide1.jpg" />
-    </slide>
-    <slide>
-      <img src="@/assets/images/slide2.jpg" />
-    </slide>
-    <slide>
-      <img src="@/assets/images/slide2.jpg" />
-    </slide>
-  </carousel>
+  <div class="container">
+    <section class="slider">
+      <carousel
+        :per-page="1"
+        :autoplay="true"
+        :loop="true"
+        :pagination-padding="5"
+        :autoplay-timeout="4000"
+      >
+        <slide>
+          <img src="@/assets/images/slide1.jpg" />
+        </slide>
+        <slide>
+          <img src="@/assets/images/slide2.jpg" />
+        </slide>
+        <slide>
+          <img src="@/assets/images/slide1.jpg" />
+        </slide>
+        <slide>
+          <img src="@/assets/images/slide2.jpg" />
+        </slide>
+        <slide>
+          <img src="@/assets/images/slide2.jpg" />
+        </slide>
+      </carousel>
+    </section>
+  </div>
 </template>
 
 <script>

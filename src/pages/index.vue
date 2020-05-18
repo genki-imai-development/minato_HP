e31ccd2c26b1f594774d63c4eddc1aecad50648c
genki-imai-development <genki-imai-development@gmail.com>
2020-05-19 01:43:01 +0900

    トップのコンテンツを追加

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -25,6 +25,58 @@
         </slide>
       </carousel>
     </section>
+    <section class="special">
+      <div class="special-inner">
+        <h2>みなとをご紹介</h2>
+        <div class="columns">
+          <div class="column is-4 special-box">
+            <a href="/helper">
+              <div class="special-box-inner">
+                <img src="@/assets/images/helper01.jpg" />
+                <p class="special-box-inner__text">
+                  みなとのヘルパーさんをご紹介！
+                </p>
+              </div>
+            </a>
+          </div>
+          <div class="column is-4 special-box">
+            <div class="special-box-inner">
+              <a href="/day">
+                <img src="@/assets/images/minato_photo1.png" />
+                <p class="special-box-inner__text">
+                  デイサービスの1日をご紹介！
+                </p>
+              </a>
+            </div>
+          </div>
+          <div class="column is-4 special-box">
+            <div class="special-box-inner">
+              <a href="/information">
+                <img src="@/assets/images/floor1004.jpg" />
+                <p class="special-box-inner__text">
+                  施設内を写真でご紹介いたします
+                </p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="feature">
+      <div class="feature-inner">
+        <h2>みなとでの過ごしかた</h2>
+        <div class="columns">
+          <div class="column is-4 special-box"></div>
+          <div class="column is-4 special-box"></div>
+          <div class="column is-4 special-box"></div>
+        </div>
+      </div>
+    </section>
+    <section class="news">
+      <div class="news-inner">
+        <h2>お知らせ</h2>
+      </div>
+    </section>
   </div>
 </template>
 <script>

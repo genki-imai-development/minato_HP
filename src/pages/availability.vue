49cc534467826575a88f3164bdbd3a5cd56f9a17
genki-imai-development <genki-imai-development@gmail.com>
2020-05-24 04:01:43 +0900

    【空き状況】ページの追加

---
+++ src/pages/availability.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container availability">
+    <section class="title">
+      <p class="title--title">料金表</p>
+      <p class="title--description">FEE TABLE</p>
+    </section>
+    <section class="availability-day">
+      <p class="availability-day--title">デイサービス みなと</p>
+      <p class="availability-day--text">
+        現在1名様の空きがございます。施設利用をご希望の方はお問い合わせください。
+      </p>
+      <p class="availability-day--note">
+        ※2020年6月1日更新
+      </p>
+    </section>
+    <section class="availability-house">
+      <p class="availability-house--title">ほほえみの家 みなと</p>
+      <p class="availability-house--text">
+        申し訳ありませんが、現在1名様の空きがございません。
+      </p>
+      <p class="availability-house--note">※2020年6月1日更新</p>
+    </section>
+  </div>
+</template>
